9858d56c3c2c216325a6507795f0b53a5470c4e4
fartem <jaman.smlnsk@gmail.com>
2020-08-24 20:13:06 +0300

    2020-08-24 Update: Added "Can we divide it?"

---
+++ codewars-c-sharp/src/Kyu8/CanWeDivideIt.cs
@@ -0,0 +1,22 @@
+﻿namespace codewars_c_sharp.src.Kyu8
+{
+    // https://www.codewars.com/kata/5a2b703dc5e2845c0900005a/train/csharp
+    class CanWeDivideIt
+    {
+        private int Number;
+        private int A;
+        private int B;
+
+        public CanWeDivideIt(int number, int a, int b)
+        {
+            Number = number;
+            A = a;
+            B = b;
+        }
+
+        public bool Result()
+        {
+            return Number % A == Number % B;
+        }
+    }
+}
---
+++ codewars-c-sharp/tests/Kyu8/CanWeDivideItTest.cs
@@ -0,0 +1,20 @@
+﻿using codewars_c_sharp.src.Kyu8;
+using NUnit.Framework;
+
+namespace codewars_c_sharp.tests.Kyu8
+{
+    [TestFixture]
+    class CanWeDivideItTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(true, new CanWeDivideIt(-12, 2, 6));
+            Assert.AreEqual(false, new CanWeDivideIt(-12, 2, -5));
+            Assert.AreEqual(false, new CanWeDivideIt(-45, 1, 6));
+            Assert.AreEqual(true, new CanWeDivideIt(45, 5, 15));
+            Assert.AreEqual(true, new CanWeDivideIt(4, 1, 4));
+            Assert.AreEqual(true, new CanWeDivideIt(15, -5, 3));
+        }
+    }
+}
