71bd403517e1c845dbb536fc86070e274e36c2bc
fartem <jaman.smlnsk@gmail.com>
2021-01-09 09:16:08 +0300

    2021-01-09 Update: Added "Name Shuffler"

---
+++ codewars-c-sharp/src/Kyu8/NameShuffler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace codewars_c_sharp.src.Kyu8
+{
+    // https://www.codewars.com/kata/559ac78160f0be07c200005a
+    public class NameShuffler
+    {
+        private string Input;
+
+        public NameShuffler(string input)
+        {
+            Input = input;
+        }
+
+        public string Solution()
+        {
+            string[] nameData = Input.Split(' ');
+            Array.Reverse(nameData);
+            return string.Join(" ", nameData);
+        }
+    }
+}
---
+++ codewars-c-sharp/tests/Kyu8/NameShufflerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using codewars_c_sharp.src.Kyu8;
+
+namespace codewars_c_sharp.tests.Kyu8
+{
+    [TestFixture]
+    public class NameShufflerTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(
+                "McClane john",
+                new NameShuffler("john McClane").Solution()
+            );
+            Assert.AreEqual(
+                "jeggins Mary",
+                new NameShuffler("Mary jeggins").Solution()
+            );
+            Assert.AreEqual(
+                "jerry tom",
+                new NameShuffler("tom jerry").Solution()
+            );
+        }
+    }
+}
