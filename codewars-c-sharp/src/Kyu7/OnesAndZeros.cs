1b6d9559b4c61e0212611db7d72d6087cbd4b250
fartem <jaman.smlnsk@gmail.com>
2021-01-14 08:26:20 +0300

    2021-01-14 Update: Added "Ones and Zeros"

---
+++ codewars-c-sharp/src/Kyu7/OnesAndZeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace codewars_c_sharp.src.Kyu7
+{
+    // https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+    public class OnesAndZeros
+    {
+        private int[] BinaryArray;
+
+        public OnesAndZeros(int[] binaryArray)
+        {
+            BinaryArray = binaryArray;
+        }
+
+        public int Solution()
+        {
+            return Convert.ToInt32(
+                string.Join("", BinaryArray),
+                2
+            );
+        }
+    }
+}
---
+++ codewars-c-sharp/tests/Kyu7/OnesAndZerosTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using codewars_c_sharp.src.Kyu7;
+
+namespace codewars_c_sharp.tests.Kyu7
+{
+    [TestFixture]
+    public class OnesAndZerosTest
+    {
+        [Test]
+        public void BasicTesting()
+        {
+            Assert.AreEqual(
+                0,
+                new OnesAndZeros(
+                    new int[] { 0, 0, 0, 0 }
+                ).Solution()
+            );
+            Assert.AreEqual(
+                15,
+                new OnesAndZeros(
+                    new int[] { 1, 1, 1, 1 }
+                ).Solution()
+            );
+            Assert.AreEqual(
+                6,
+                new OnesAndZeros(
+                    new int[] { 0, 1, 1, 0 }
+                ).Solution()
+            );
+            Assert.AreEqual(
+                5,
+                new OnesAndZeros(
+                    new int[] { 0, 1, 0, 1 }
+                ).Solution()
+            );
+        }
+    }
+}
